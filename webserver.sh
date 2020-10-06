c6451a68f69e480dbe42cacc2dc55c7f4f898653
audiazzura23 <35802103+audiazzura23@users.noreply.github.com>
2020-10-06 18:28:54 +0700

    added restart apache command
    added restart apache command

--- webserver.sh
+++ webserver.sh
@@ -6,6 +6,9 @@ apt-get -y install apache2
 apt-get -y install mysql-server 
 apt-get -y install php php-mysql libapache2-mod-php php-cli
 
+#restart apache
+service apache2 restart
+
 #mengcopy file index.php ke direktori root apache
 rm -rf /var/www/html/index.html
 cp index.php /var/www/html
