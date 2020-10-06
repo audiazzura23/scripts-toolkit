c43f59796983497e5a26b6534380dc94b1f73fa3
audiazzura23 <35802103+audiazzura23@users.noreply.github.com>
2020-10-06 17:37:53 +0700

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -3,7 +3,7 @@
 #instalasi paket yang dibutuhkan
 apt update
 apt-get -y install apache2
-apt-get install php7.4-cli
+apt-get install php-cli
 
 #mengcopy file index.php ke folder root apache
 rm -rf /var/www/html
