bc7865576b52e381ffedc102405d553b8c867f37
audiazzura23 <35802103+audiazzura23@users.noreply.github.com>
2020-10-06 18:22:52 +0700

    Update webserver.sh
    Full LAMP Stack Install

--- webserver.sh
+++ webserver.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#instalasi paket yang dibutuhkan
+#instalasi paket yang dibutuhkan (LAMP Stack)
 apt update
-apt-get -y install apache2
-apt-get install php-cli
+apt-get -y install apache2 
+apt-get -y install mysql-server 
+apt-get -y install php php-mysql libapache2-mod-php php-cli
 
-#mengcopy file index.php ke folder root apache
-rm -rf /var/www/html
+#mengcopy file index.php ke direktori root apache
+rm -rf /var/www/html/index.html
 cp index.php /var/www/html
