e1e5585135038a89ed127142646d6a2598e42919
audiazzura23 <35802103+audiazzura23@users.noreply.github.com>
2020-10-18 01:51:16 +0700

    major update

--- wordpress.sh
+++ wordpress.sh
@@ -1,70 +1,61 @@
 #!/bin/bash
 
-#cek direktori
-pwd=$(pwd) 
+#get current directory
+pwd=$(pwd)
 
-#input root password  mysql 
-read -p 'wordpress_db_name [wp_db]: ' wordpress_db_name  
-read -p 'db_root_password [only-alphanumeric]: ' db_root_password  
-echo 
+#variables to be used for db
+read -p 'wordpress_db_name [wp_db]: ' wordpress_db_name
+read -p 'db_root_username [only-alphanumeric]: ' db_root_username
+read -p 'db_root_password [only-alphanumeric]: ' db_root_password
+read -p 'db_host [for default, type:localhost]: ' db_host
+echo
 
-#instalasi paket yang dibutuhkan (LAMP Stack)
-apt-get -y update
-apt-get -y install apache2 apache2-utils
-apt-get -y install mysql-server 
-apt-get -y install php php-mysql libapache2-mod-php php-cli
+#lamp stack installation
+apt -y update
+apt -y install apache2
+apt -y install mysql-server
+apt-get -y install php php-mysql libapache2-mod-php php-cli php-cgi php-gd
 
-#jalankan apache
-systemctl start apache2  
-systemctl enable apache2  
-   
-#install MySQL database server  
-export DEBIAN_FRONTEND="noninteractive"  
-debconf-set-selections <<< "mysql-server mysql-server/root_password password $db_root_password"  
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $db_root_password"  
-apt-get install mysql-server mysql-client -y  
-   
-#install Latest WordPress  
-rm /var/www/html/index.*  
-wget -c http://wordpress.org/latest.tar.gz  
-tar -xzvf latest.tar.gz  
-rsync -av wordpress/* /var/www/html/  
-   
+#makes sure lamp stack running
+systemctl start apache2
+systemctl start mysql
+
+#mysql secure install 
+mysql --user=root <<_EOF_
+UPDATE mysql.user SET authentication_string = PASSWORD('$db_root_password') WHERE User='root';
+UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+_EOF_
+
+#wordpress installation
+rm /var/www/html/index.*
+apt -y install wget
+wget https://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+cp -r wordpress/* /var/www/html
+
+#apache restart
+systemctl restart apache2
+
 #set kepemilikan dan permissions  
-chown -R www-data:www-data /var/www/html/  
-chmod -R 755 /var/www/html/  
-   
-#konfigurasi database wordpress
-mysql -uroot -p$db_root_password <<QUERY_INPUT  
-CREATE DATABASE $wordpress_db_name;  
-GRANT ALL PRIVILEGES ON $wordpress_db_name.* TO 'root'@'localhost' IDENTIFIED BY '$db_root_password';  
-FLUSH PRIVILEGES;  
-EXIT  
-QUERY_INPUT  
-   
-#input data db ke config wordpress
-cd /var/www/html/  
-sudo mv wp-config-sample.php wp-config.php  
-perl -pi -e "s/database_name_here/$wordpress_db_name/g" wp-config.php  
-perl -pi -e "s/username_here/root/g" wp-config.php  
-perl -pi -e "s/password_here/$db_root_password/g" wp-config.php  
-   
-# Enabling Mod Rewrite  
-a2enmod rewrite  
-php5enmod mcrypt  
-   
-#instalasi phpmyadmin
-apt-get install phpmyadmin -y  
-   
-#konfigurasi phpmyadmin
-echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf  
-   
-#restart apache dan mysql
-service apache2 restart  
-service mysql restart  
+chown -R www-data:www-data /var/www/
+
+#mysql config for wordpress
+mysql -u root -p $db_root_password <<QUERY_INPUT
+CREATE DATABASE $wordpress_db_name;
+CREATE USER "$wordrpress_db_username"@"db_host" IDENTIFIED BY '$db_root_password';
+GRANT ALL PRIVILEGES ON $wordpress_db_name.* TO '$wordpress_db_username'@'db_host' IDENTIFIED BY '$db_root_password';
+FLUSH PRIVILEGES;
+EXIT;
+QUERY_INPUT
    
 #cleaning
 cd $pwd  
 rm -rf latest.tar.gz wordpress  
    
-echo "Installation is Complete"  
+echo "Installation is Complete"
+echo
